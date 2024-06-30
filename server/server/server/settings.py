17adc9a896dddbedc73b4735dae860b72619dbc6
Laurens De Wachter <93316351+Laurensdewachter@users.noreply.github.com>
2024-06-30 04:31:05 +0200

    Setup database and connection

--- server/server/server/settings.py
+++ server/server/server/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +77,12 @@ WSGI_APPLICATION = "server.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("SERVER_DB_NAME"),
+        "USER": os.environ.get("SERVER_DB_USER"),
+        "PASSWORD": os.environ.get("SERVER_DB_PASSWORD"),
+        "HOST": os.environ.get("SERVER_DB_HOST"),
+        "PORT": os.environ.get("SERVER_DB_PORT"),
     }
 }
 
