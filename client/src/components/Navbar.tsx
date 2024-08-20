dae06d74af24209ce7d8c9470de6c88627915f43
Laurens De Wachter <93316351+Laurensdewachter@users.noreply.github.com>
2024-08-20 14:41:36 +0200

    Add account icon

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,21 +1,22 @@
 import checkLoginStatus from "../requests/authentication/status";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Person } from "react-bootstrap-icons";
 import "./Navbar.css";
 
 function CustomNavbar() {
-  const loginStatus = checkLoginStatus();
+  const loggedIn = checkLoginStatus();
 
   function getUserComponents() {
-    if (loginStatus) {
+    if (loggedIn) {
       // User is logged in
       return (
-        <Nav className="me-1 mb-4 mb-lg-0">
+        <Nav className="ms-4 me-4 mb-4 me-lg-1 mb-lg-0">
           <Nav.Link href="/" className="me-3 mb-1 btn-login">
             Tuyaux
           </Nav.Link>
-          <Nav.Link href="/account" className="me-3 btn-register">
-            Mijn account
+          <Nav.Link href="/account" className="me-3 btn-account">
+          <Person className="me-1 icon-person"/>Mijn Account
           </Nav.Link>
         </Nav>
       );
@@ -40,7 +41,7 @@ function CustomNavbar() {
         <img alt="WINAK" src="/schild.svg" width="100" height="100" />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" className="me-4" />
-      <Navbar.Collapse className="ms-4">
+      <Navbar.Collapse>
         <Nav className="me-auto mb-3 mb-lg-0">
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/over-ons">Over ons</Nav.Link>
